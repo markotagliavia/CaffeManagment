b38284e95c0601c84f38d33fdf708b2120af020e
Tijana994 <tijana.vdn@gmail.com>
2018-12-09 01:01:23 +0100

    Helper class

---
+++ CaffeManagment/CaffeManagment/SecurityManager/Audit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeManagment.SecurityManager
+{
+    public class AuditManager
+    {
+
+        static AuditManager()
+        {
+
+        }
+
+        public static void AuditToDB(string user, string akcija, string tip)
+        {
+            DateTime dt = DateTime.Now;
+            
+        }
+    }
+}
---
+++ CaffeManagment/CaffeManagment/SecurityManager/AuthorizationPolicy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeManagment.SecurityManager
+{
+    
+    public enum Permission
+    {
+        Login,
+        ResetPassword
+    }
+
+    public enum Role : int
+    {
+        Admin = 1,
+        Employee = 2
+    }
+
+    public class AuthorizationPolicy
+    {
+        private static  Dictionary<Role, List<Permission>> RolePermission;
+
+        static AuthorizationPolicy()
+        {
+            RolePermission = new Dictionary<Role, List<Permission>>();
+        }
+
+        public static bool HavePermission(int korisnik_id, Permission p)
+        {
+
+            //string permission = convertToString(p);
+            //if (dbContext.Korisniks.Any(x => x.id == korisnik_id && x.active == true))
+            //{
+            //    if (p.Equals(Permission.Login) || p.Equals(Permission.ResetPassword))
+            //    {
+            //        if (dbContext.Korisniks.Any(x => x.id == korisnik_id && x.ulogovan == true))
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (dbContext.Korisniks.Any(x => x.id == korisnik_id && x.ulogovan == false))
+            //        {
+            //            return false;
+            //        }
+            //    }
+
+
+            //    if (dbContext.Permissions.Any(x => x.naziv.Equals(permission)))
+            //    {
+            //        int id_zaposleni = dbContext.Korisniks.First(x => x.id == korisnik_id && x.active == true).zaposleni_id;
+            //        if (dbContext.Zaposlenis.Any(x => x.id == id_zaposleni && x.active == true))
+            //        {
+            //            ICollection<Common.Model.Uloga> uloge = dbContext.Zaposlenis.First(x => x.id == id_zaposleni && x.active == true).Ulogas;
+            //            foreach (Uloga u in uloge)
+            //            {
+            //                if (u.Permissions.Any(x => x.naziv.Equals(permission)))
+            //                {
+            //                    return true;
+            //                }
+            //            }
+            //            return false;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return false;
+        }
+
+        public static String convertToString(Permission p)
+        {
+            return Permission.GetName(p.GetType(), p);
+        }
+    }
+   
+}
